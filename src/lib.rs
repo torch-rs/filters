d2282b097a811e9e9f43d7fa977c31d1fa783c9a
Patrick Chan <patrickchan1999@gmail.com>
2018-07-26 20:04:12 -0400

    Change Vec<String> to Vec<SearchCandidate> in preparation for icons

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
+extern crate search_candidate;
+
 pub mod ignore_case_prefix_filter;
 pub mod ignore_case_substring_filter;
 pub mod prefix_filter;
 pub mod substring_filter;
 
+use self::search_candidate::SearchCandidate;
+
 pub trait Filter {
 
-    fn filter(candidates: Vec<String>, search_term: String) -> Vec<String>;
+    fn filter(candidates: Vec<SearchCandidate>, search_term: String) -> Vec<SearchCandidate>;
 
 }
